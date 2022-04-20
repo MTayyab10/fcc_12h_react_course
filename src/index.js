9346c2b90651de7b45e34f558d47309f3b450f20
Tayyab <tayyabm10@hotmail.com>
2022-04-20 16:55:57 +0800

    start to learn react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom"
+import Navbar from "./Navbar";
+
+
+ReactDOM.render(
+    <div>
+        <Navbar/>
+    </div>,
+    document.getElementById('root')
+)
