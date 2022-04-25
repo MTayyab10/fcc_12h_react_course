f526edc4823cbcbf4f69befd390d422c204d781b
Tayyab <tayyabm10@hotmail.com>
2022-04-25 20:59:26 +0800

    project 1, Fun-Facts complete

---
+++ src/Project1 - React FunFacts/app.js
@@ -0,0 +1,15 @@
+import Navbar from "./navbar";
+import FunFacts from "./fun_facts";
+import Footer from "./footer";
+
+export default function MyApp() {
+
+    return (
+        <div>
+            <Navbar/>
+            <FunFacts />
+            <Footer />
+
+        </div>
+    )
+}
